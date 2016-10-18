ac1bb80b60f097f717b67edbc5721b498ee286e2
Kristo <kristopalo@hotmail.com>
2016-10-18 20:37:57 +0300

    forwarding first draft

--- src/Handlers.java
+++ src/Handlers.java
@@ -49,13 +49,6 @@ public class Handlers extends SimpleHttpServer {
             os.write(response.getBytes());
             os.close();
 
-            if (he.getRequestMethod().equals("GET")) {
-                System.out.println("Received a request from " + he.getRequestHeaders().getFirst("Host"));
-                new GetHandler().handle(he);
-            } else {
-                new PostHandler().handle(he);
-            }
-
             if (parameters.get("id") != null && parameters.get("url") != null) {
                 Request request = new Request(parameters.get("id").toString(), parameters.get("url").toString());
                 getIn().put(parameters.get("id").toString(), request);
