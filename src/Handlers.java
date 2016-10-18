51310c279ebb16a4dec857ef91ea19869008fe77
eekripalo <kristopalo@hotmail.com>
2016-10-18 18:15:33 +0300

    old again

--- src/Handlers.java
+++ src/Handlers.java
@@ -53,42 +53,41 @@ public class Handlers extends SimpleHttpServer {
                 Request request = new Request(parameters.get("id").toString(), parameters.get("url").toString());
                 getIn().put(parameters.get("id").toString(), request);
 
-                //double d = new Random().nextDouble();
-                // if (d <= 0.50) {
-                //TODO download file with and construct /file post message
+                double d = new Random().nextDouble();
+                if (d <= 0.50) {
+                    //TODO download file with and construct /file post message
 
-                //} else {
-                //TODO do not download, but send request to everyone else in the network
-                Set set = getPeers().entrySet();
+                } else {
+                    //TODO do not download, but send request to everyone else in the network
+                    Set set = getPeers().entrySet();
 
-                Iterator iterator = set.iterator();
-                while (iterator.hasNext()) {
-                    Map.Entry me = (Map.Entry) iterator.next();
-                    if (!((Neighbor) me.getValue()).isAlive()) {
-                        continue;
+                    Iterator iterator = set.iterator();
+                    while(iterator.hasNext()) {
+                        Map.Entry me = (Map.Entry)iterator.next();
+                        if (!((Neighbor) me.getValue()).isAlive()) {
+                            continue;
+                        }
+                        URL url = new URL(((Neighbor) me.getValue()).getIp() + ":" + ((Neighbor) me.getValue()).getPort() + "?" + "id=" + parameters.get("id").toString() + "&" + "url=" + parameters.get("url").toString());
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setRequestMethod("GET");
+                        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                        System.out.println("Forwarded message to " + ((Neighbor) me.getValue()).getIp() + ":" + ((Neighbor) me.getValue()).getPort());
+
+                        String line;
+                        StringBuilder result = null;
+
+                        while ((line = rd.readLine()) != null) {
+                            result.append(line);
+                        }
+                        rd.close();
+
+                        System.out.println("Received a reply from " + ((Neighbor) me.getValue()).getIp() + ":" + ((Neighbor) me.getValue()).getPort());
+
                     }
-                    URL url = new URL(((Neighbor) me.getValue()).getIp() + ":" + ((Neighbor) me.getValue()).getPort() + "?" + "id=" + parameters.get("id").toString() + "&" + "url=" + parameters.get("url").toString());
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("GET");
-                    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    System.out.println("Forwarded message to " + ((Neighbor) me.getValue()).getIp() + ":" + ((Neighbor) me.getValue()).getPort());
-
-                    String line;
-                    StringBuilder result = null;
-
-                    while ((line = rd.readLine()) != null) {
-                        result.append(line);
-                    }
-                    rd.close();
-
-                    System.out.println("Received a reply from " + ((Neighbor) me.getValue()).getIp() + ":" + ((Neighbor) me.getValue()).getPort());
-
-                    //}
 
                 }
 
                 if (he.getRequestMethod().equals("GET")) {
-                    System.out.println("Request URI: " + he.getRequestURI());
                     new GetHandler().handle(he);
                 } else {
                     new PostHandler().handle(he);
@@ -97,9 +96,9 @@ public class Handlers extends SimpleHttpServer {
             }
 
 
+
         }
     }
-
 
     public static class HeaderHandler implements HttpHandler {
 
