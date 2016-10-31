b6e8c2ab94c9e2ac40d8e103f151d83a854f5dad
Kristo <kristopalo@hotmail.com>
2016-10-31 16:07:40 +0200

    working copy

--- src/FileResponseHandler.java
+++ src/FileResponseHandler.java
@@ -1,7 +1,12 @@
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Kristo on 31.10.2016.
@@ -11,6 +16,57 @@ public class FileResponseHandler extends SimpleHttpServer implements HttpHandler
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         System.out.println("Received a file response");
+
+        Map<String, Object> parameters = new HashMap<>();
+        InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+        BufferedReader br = new BufferedReader(isr);
+        String query = br.readLine();
+        parseQuery(query, parameters);
+        // send response
+        String response = "";
+        for (String key : parameters.keySet())
+            response += key + " = " + parameters.get(key) + "\n";
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.toString().getBytes());
+        os.close();
+
+    }
+
+    public void parseQuery(String query, Map<String, Object> parameters) throws UnsupportedEncodingException {
+
+        if (query != null) {
+            String pairs[] = query.split("[&]");
+
+            for (String pair : pairs) {
+                String param[] = pair.split("[=]");
+
+                String key = null;
+                String value = null;
+                if (param.length > 0) {
+                    key = URLDecoder.decode(param[0], System.getProperty("file.encoding"));
+                }
+
+                if (param.length > 1) {
+                    value = URLDecoder.decode(param[1], System.getProperty("file.encoding"));
+                }
+
+                if (parameters.containsKey(key)) {
+                    Object obj = parameters.get(key);
+                    if (obj instanceof List<?>) {
+                        List<String> values = (List<String>) obj;
+                        values.add(value);
+                    } else if (obj instanceof String) {
+                        List<String> values = new ArrayList<String>();
+                        values.add((String) obj);
+                        values.add(value);
+                        parameters.put(key, values);
+                    }
+                } else {
+                    parameters.put(key, value);
+                }
+            }
+        }
     }
 
 }
